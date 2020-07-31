17b71fee9d501af82ef8741f40cb0ebce8b45c0c
admin <admin@admins-MacBook-Air.local>
2020-07-31 16:37:19 +1000

    Initial commit

---
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name",
+    required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+    @GetMapping("/greeting2")
+    public String greeting2(@RequestParam(name = "name",
+            required = false, defaultValue = "People") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting2";
+    }
+
+}
